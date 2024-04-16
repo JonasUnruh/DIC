9031e6f31adb011fce736baf10ef0aea63bb8b68
Leon <l.beccard@protonmail.com>
2024-04-16 12:09:53 +0200

    Add server version to bash file

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 INPUT=./reviews_devset.json
 
 # Run the preprocess_runner.py to preprocess the input file
+# Server version
+# python preprocess_runner.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop $INPUT > temp.txt
+# Local version
 python preprocess_runner.py $INPUT > temp.txt
 
+
 # Run chi_square.py to calculate the chi-square value
+# Server version
+# python --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop chi_square.py temp.txt > output.txt
+# Local version
 python chi_square.py temp.txt > output.txt
