049736b0b1f020d9072c3462a463bd72ff8f6c6b
Simon Tritscher <simon.tritscher@student.tuwien.ac.at>
2024-04-16 12:05:41 +0200

    fix script with local devset as source

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-INPUT=hdfs:///e11701226/dic24_shared/amazon-reviews/full/reviewscombined.json
+INPUT=./reviews_devset.json
 
 # Run the preprocess_runner.py to preprocess the input file
-python preprocess_runner.py -r hadoop $INPUT > temp.txt
+python preprocess_runner.py $INPUT > temp.txt
 
 # Run chi_square.py to calculate the chi-square value
-python chi_square.py -r hadoop temp.txt > output.txt
+python chi_square.py temp.txt > output.txt
