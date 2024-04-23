4c31cf89adbfd290bc20ec1b121fefb5d53c4fe9
Leon <l.beccard@protonmail.com>
2024-04-23 12:13:20 +0200

    Add several INPUTs

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
-INPUT=./reviews_devset.json
+INPUT=hdfs:///user/dic24_shared/amazon-reviews/full/reviews_devset.json
+
+#Full dataset
+#INPUT=hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json
 
 # Run the preprocess_runner.py to preprocess the input file
 # Server version
-# python preprocess_runner.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop $INPUT > temp.txt
+python preprocess_runner.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop $INPUT > temp.txt
 # Local version
-python preprocess_runner.py $INPUT > temp.txt
+#python preprocess_runner.py $INPUT > temp.txt
 
 
 # Run chi_square.py to calculate the chi-square value
 # Server version
-# python --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop chi_square.py temp.txt > output.txt
+python --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop chi_square.py temp.txt > output.txt
 # Local version
-python chi_square.py temp.txt > output.txt
+#python chi_square.py temp.txt > output.txt
